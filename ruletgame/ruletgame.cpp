39c750887d1a2e61c152f4fc2056c94691722476
huseaky <155255751+huseaky@users.noreply.github.com>
2024-01-01 23:47:07 +0300

    rulet oyun
    son halz

--- ruletgame/ruletgame.cpp
+++ ruletgame/ruletgame.cpp
@@ -1,68 +1,80 @@
 #include <iostream>
+#include <random>
 using namespace std;
 int main()
 {
-    int anaPara = 1;
-    int bahis;
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    int anaPara = 100;
+    int bahis = 100;
     int secim;
-    while (true)
-    {
-        cout << "oynanacak para gir" << endl;
-        cin >> bahis;
-        while (anaPara < bahis)
-        {
-            cout << "oynanacak para gir" << endl;
-            cin >> bahis;
-        }
-        cout << "oynanacak sayiyi gir" << endl;
-        cin >> secim;
-        while (secim > 3 || secim < 1)
-        {
-            cout << "tekrara sayi gir" << endl;
-            cin >> secim;
-        }
-        
+    int oyun = 0;
 
-        int rastgeleSayi;
-        srand(time(NULL));
-        rastgeleSayi = rand() % 100;
+    cout << "oynanacak sayiyi gir" << endl;
+    cin >> secim;
+
+    while (secim > 3 || secim < 1)
+    {
+        cout << "tekrara sayi gir" << endl;
+        cin >> secim;
+    }
+
+    while (anaPara > 0)
+    {
+       std::uniform_int_distribution<int> dist(0, 100);
+       
 
         int gelen;
-        if (rastgeleSayi < 50)
+        if (dist(mt) < 50)
         {
             gelen = 1;
         }
-        else if (rastgeleSayi > 50)
+        else if (dist(mt) > 50)
         {
             gelen = 2;
         }
-        else if (rastgeleSayi == 50)
+        else if (dist(mt) == 50)
         {
             gelen = 3;
         }
         cout << "sounc:" << gelen << endl;
+
         if (gelen == secim && gelen != 3)
         {
             cout << "kazandin:" << bahis * 2 << endl;
             anaPara += bahis * 2;
+            bahis = 100;
+            oyun++;
+            
         }
+
         else if (gelen == secim && gelen == 3)
         {
             cout << "kazandins" << bahis * 14 << endl;
             anaPara += bahis * 14;
+            bahis = 100;
+            oyun++;
         }
+
         else
         {
             cout << "kaybettin" << endl;
             anaPara -= bahis;
+            bahis *= 2;
+            oyun++;
+            secim=gelen;
         }
+
         cout << "Yeni bakiye:" << anaPara << endl;
-        if(anaPara<=0){
-            cout<<"siktgir git";
+
+        if (anaPara <= 0)
+        {
+            cout << "siktgir git"
+                 << "oynadgn oyun: " << oyun << endl;
             break;
         }
     }
 
-    // if(dfgsdşlöfmötş<as.lkjih<dzertpğlmhzğ<eapodolkmnö<)
+    // if(dfgsdşlöfmötş<as.lkjih<dzertpğlmhzğ<easpodolkmnö<)
     return 0;
 }
